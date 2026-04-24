60e43bb8a891dfbe2b775c4ce508e21d3a415848
Thiagosoca22 <THIAGO.SOCA@correo.ucu.edu.uy>
2026-04-23 22:59:21 -0300

    Agregando el codigo de la clase

--- src/main/java/uy/edu/curso/classes/RegistroDeTransplante.java
+++ src/main/java/uy/edu/curso/classes/RegistroDeTransplante.java
@@ -1,5 +1,17 @@
 package uy.edu.curso.classes;
 
-public class RegistroDeTransplante {
+public class RegistroTransplante {
 
+    private String nombreReceptor;
+    private String organo;
+
+    public RegistroTransplante(String nombreReceptor, String organo) {
+        this.nombreReceptor = nombreReceptor;
+        this.organo = organo;
+    }
+
+    @Override
+    public String toString() {
+        return "Transplante: " + organo + " a " + nombreReceptor;
+    }
 }
